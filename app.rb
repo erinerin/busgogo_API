3d8f072009bda70c83aef5b8c6b88b95e9128dc8
wei <wei.yen.0718gmail.com>
2014-11-06 01:05:23 +0800

    class bus123

--- app.rb
+++ app.rb
@@ -3,3 +3,21 @@ require 'sinatra/namespace'
 require 'busgogo'
 require 'json'
 
+class bus123 < sinatra::base
+	register sinatra::namespace
+
+
+helpers do
+	def station
+		budges_after = {
+			'station' => station
+		}
+	end
+end
+
+
+
+
+
+end
+
