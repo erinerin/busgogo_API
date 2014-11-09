942ef705c0a691fddb28156db78f8145adb23b8c
erinerin <jolin_2121@hotmail.com.tw>
2014-11-09 19:03:14 +0800

    get ok

--- app.rb
+++ app.rb
@@ -12,32 +12,11 @@ helpers do
  			'name' => station,
 			'profiles' => []
 			}
-
-			name = params[:station]
-
 			scmachine.busstation.each do |value|
 			profile_after['profiles'].push('sta' => value)
-
 			end
  			@profile_after
 	end
-
-
-
-	def check_station(station,profiles)
-		@check_station={}
-		station.each do |sta|
-			found= busgogo::WebScraper.tmp_selectstation.keys
-			@check_station[sta] =\
-			profiles.select{ |profile| !profiles_found.include? profile }
-
-		end
-	@check_station
-	end
-
-
-
-
 end
 
 get '/api/v1/station/:station.json' do
@@ -45,23 +24,9 @@ get '/api/v1/station/:station.json' do
 	get_profile(params[:station]).to_json
 end
 
-
-post '/api/v1/check' do
-content_type :json
-req=JSON.parse(request.body.read)
-station = req['station']
-profiles= req['profiles']
-check_station(station, profiles).to_json
-
-
-
-end
-
 get '/' do
 'Simplecadet api/v1 is up and working'
 end
-
-
 
 end
 
