a49c953b0b4f1e7d1c1988e00ee150be13f122f4
wei <wei.yen.0718gmail.com>
2014-11-09 17:48:07 +0800

    post

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class Bus < Sinatra::Base
 helpers do
 	def get_profile(station)
         	scmachine = WebScraper.new
-			bus_station=scmachine.busstation
+			
 
 			profile_after={
  			'name' => station,
@@ -15,19 +15,63 @@ helpers do
 			}
 
 			name = params[:station]
-			bus_station.profile(name)[0].each do |value|
+			scmachine.busstation.each do |value|
 			profile_after['profiles'].push('sta' => value)
 			end
  			profile_after
         
 	end
+
+	def get_data
+        	scmachine = WebScraper.new
+			
+
+			profile_after={
+ 			'name' => station,
+			'profiles' => []
+			}
+
+			
+			scmachine.tmp_selectstation.each do |value|
+			profile_after['profiles'].push('sta' => value)
+			end
+ 			profile_after
+        
+	end
+
+	def check_station(station,profiles)
+		@check_station={}
+		station.each do |sta|
+			found= busgogo::WebScraper.tmp_selectstation.keys
+			@check_station[sta] =\
+			profiles.select{ |profile| !profiles_found.include? profile }
+
+		end
+	@check_station
+	end
+
+
+
+
 end
 
 get '/api/v1/station/:station.json' do
+	content_type :json
+	get_profile(params[:station]).to_json
+end
+
+
+post '/api/v1/check' do
 content_type :json
-get_profile(params[:station]).to_json
+req=JSON.parse(request.body.read)
+station = req['station']
+profiles= req['profiles']
+check_station(station, profiles).to_json
+
 
 
 end
+
+
 end
 
