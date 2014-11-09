bb24cbf0cded0375d3bc90d4c0d754948ffbe522
wei <wei.yen.0718gmail.com>
2014-11-09 22:42:12 +0800

    Added busgogo gem

--- app.rb
+++ app.rb
@@ -1,31 +1,31 @@
 require 'sinatra/base'
-# require 'busgogo'
+require 'busgogo'
 require 'json'
 
 class Bus < Sinatra::Base
 	
-# helpers do
-# 	def get_profile(station)
-#         	scmachine = WebScraper.new
+helpers do
+	def get_profile(station)
+        	scmachine = WebScraper.new
 			
 
-# 			profile_after={
-#  			'name' => station,
-# 			'profiles' => []
-# 			}
+			profile_after={
+ 			'name' => station,
+			'profiles' => []
+			}
 
-# 			scmachine.busstation.each do |value|
-# 			profile_after['profiles'].push('sta' => value)
+			scmachine.busstation.each do |value|
+			profile_after['profiles'].push('sta' => value)
 
-# 			end
-#  			profile_after
+			end
+ 			profile_after
         
-# 	end
-# end
+	end
+end
 
 get '/api/v1/station/:station.json' do
-	# content_type :json
-	# get_profile(params[:station]).to_json
+	content_type :json
+	get_profile(params[:station]).to_json
 end
 
 get '/' do
